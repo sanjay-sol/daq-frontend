3d9bc99ab7f8e7e9f5ef743ba308c70f5209ffca
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-23 01:37:33 +0530

    added profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
 
-const page = () => {
+import ProfileHeader from "../../components/shared/ProfileHeader";
+
+async function Page() {
+
+  //* fetch userData from the server
+
+  const userInfo = {
+    id: "1",
+    name: "Sanjay Sirangi",
+    username: "sanjay-sol",
+    image:"/assets/user.svg",
+    bio: "I am a developer and I love problem solving and building things.",
+  };
+
   return (
-    <>
-      <div>Profile</div>
-      
-    </>
-  )
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        image={userInfo.image}
+        bio={userInfo.bio}
+      />
+    </section>
+  );
 }
-
-export default page
+export default Page;
