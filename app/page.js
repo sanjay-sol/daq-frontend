72d8e2595cfcd32e34e7bc0e2c2a67b289643413
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-23 01:55:13 +0530

    added button

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import { Button } from "../components/ui/button";
 
-const page = () => {
+const App = () => {
+  const url = "https://github.com/sanjay-sol/nextjs-frontend-template";
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Button type="submit" className="bg-primary-500">
+      <Link href={url} target="_blank" >
+         Source Code
+      </Link>
+      </Button>
+    </div>
+  );
+};
 
-export default page
+export default App;
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -38,7 +38,7 @@ const Button = (
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={buttonVariants({ variant, size, className })}
         ref={ref}
         {...props}
       />
